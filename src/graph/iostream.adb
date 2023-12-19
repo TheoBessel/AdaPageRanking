633a4bce1a77c88b9106341cdc6e211cd6073891
LEBOBE Timothe <tle2734@griffon.enseeiht.fr>
2023-12-19 13:53:53 +0100

    remplacement case if elsif else

---
+++ src/graph/iostream.adb
@@ -0,0 +1,251 @@
+-- Implémantation du module IOStream
+with Ada.Text_IO;			use Ada.Text_IO;
+
+
+
+package body IOStream is
+
+    function Parse_Args (args : in T_Args) return T_Constantes is
+        constantes : T_Constantes;                  -- record qui stocke les constantes
+        alpha_non_initialisee : Boolean := True;    -- si alpha     n'est pas initialisée
+        k_non_initialisee : Boolean := True;        -- si K         n'est pas initialisée
+        eps_non_initialisee : Boolean := True;      -- si epsilon   n'est pas initialisée
+        mode_non_initialisee : Boolean := True;     -- si mode      n'est pas initialisée
+        prefix_non_initialisee : Boolean := True;   -- si prefixe   n'est pas initialisée
+        passer_parametre : Boolean := False;        -- si le paramètre n'es pas à analyser
+    begin
+        for J in 1..args.V_Count loop
+            if passer_parametre then
+                passer_parametre := False;
+            else
+                case args.V_Args(J) is
+
+                    when To_Unbounded_String("-A") =>
+                        
+                        if not alpha_non_initialisee then
+                            raise Bad_Arguments_Exception with "Double définition de alpha en argument";
+                        else
+                            null;
+                        end if;
+
+                        alpha_non_initialisee := False;
+                        passer_parametre := True;
+
+                        if J+1 > args.V_Count then 
+                            raise Bad_Arguments_Exception with "Aucune valeur de Alpha donné en argument";
+                        else
+                            null;
+                        end if;
+
+                        begin
+                            Constantes.alpha := T_Float'Value(To_String(args.V_Args(J + 1)));
+                            if Constantes.alpha < 0.0 or 1.0 < Constantes.alpha then
+                                raise Bad_Arguments_Exception with "L'argument qui suit -A n'est pas  compris entre 0 et 1";
+                            else
+                                null;
+                            end if;
+                        exception
+                            when others =>
+                                raise Bad_Arguments_Exception with "L'argument qui suit -A n'est pas un float";
+                        end;
+
+                    when To_Unbounded_String("-K") =>
+                        -- vérification que K n'a pas déjà était définie
+                        if not k_non_initialisee then
+                            raise Bad_Arguments_Exception  with "Double définition de k en argument";
+                        else
+                            null;
+                        end if;
+
+                        k_non_initialisee := False;
+                        passer_parametre := True;
+                        
+                        if J+1 > args.V_Count then 
+                            raise Bad_Arguments_Exception with "Aucune valeur de k donné en argument";
+                        else
+                            null;
+                        end if;
+                        begin
+                            Constantes.k := Natural'Value(To_String(args.V_Args(J + 1)));
+                        exception
+                            when others =>
+                                raise Bad_Arguments_Exception with "L'argument qui suit -K n'est pas un entier Naturel";
+                        end;
+                
+                    when To_Unbounded_String("-E") =>
+                        -- vérification que eps n'a pas déjà était définie
+                        if not eps_non_initialisee then
+                            raise Bad_Arguments_Exception with "Double définition de eps en argument";
+                        else
+                            null;
+                        end if;
+
+                        eps_non_initialisee := False;
+                        passer_parametre := True;
+
+                        if J+1 > args.V_Count then 
+                            raise Bad_Arguments_Exception with "Aucune valeur de eps donné en argument";
+                        else
+                            null;
+                        end if;
+
+                        begin
+                            Constantes.eps := T_Float'Value(To_String(args.V_Args(J + 1)));
+                            if Constantes.eps < 0.0 then
+                                raise Bad_Arguments_Exception with "L'arguement qui suit -E doit être un Float positif";
+                            else
+                                null;
+                            end if;
+                        exception
+                            when others =>
+                                raise Bad_Arguments_Exception with "L'argument qui suit -E n'est pas un Float";
+                        end;
+
+                    when To_Unbounded_String("-P") =>
+                        if not mode_non_initialisee then
+                            raise Bad_Arguments_Exception with "Le mode a été choisi 2 fois";
+                        else
+                            null;
+                        end if;
+
+                        mode_non_initialisee := False;
+                        
+                        Constantes.mode := Pleine;
+
+
+                    when To_Unbounded_String("-C") =>
+                        if not mode_non_initialisee then
+                            raise Bad_Arguments_Exception with "Le mode a été choisi 2 fois";
+                        else
+                            null;
+                        end if;
+
+                        mode_non_initialisee := False;
+
+                        Constantes.mode := Creuse;
+
+                    when To_Unbounded_String("-R") =>
+                        if not prefix_non_initialisee then
+                            raise Bad_Arguments_Exception with "Le préfixe de sortie a déjà été initialisé";
+                        else
+                            null;
+                        end if;
+
+                        prefix_non_initialisee := False;
+                        passer_parametre := True;
+
+                        if J+1 > args.V_Count then 
+                            raise Bad_Arguments_Exception with "Aucune valeur de prefixe donné en argument";
+                        else
+                            null;
+                        end if;
+
+                        Constantes.prefixe := args.V_Args(J + 1);
+
+                    when others =>
+                        raise Bad_Arguments_Exception with "L'argument " & To_String(args.V_Args(J)) & " n'est pas reconnu par le programme";
+                end case;
+            end if;
+        end loop;
+
+        -- Initialisation des constantes non initialisée
+        if alpha_non_initialisee then
+            Constantes.alpha := 0.85;
+        else
+            null;
+        end if;
+        
+        if k_non_initialisee then
+            Constantes.k := 150;
+        else
+            null;
+        end if;
+        
+        if prefix_non_initialisee then
+            Constantes.prefixe := To_Unbounded_String("output");
+        else
+            null;
+        end if;
+
+        if eps_non_initialisee then
+            Constantes.eps := 0.0;
+        else
+            null;
+        end if;
+        
+        
+        if mode_non_initialisee then
+            Constantes.mode := Creuse;
+        else
+            null;
+        end if;         
+
+        return Constantes;
+    end Parse_Args;
+
+
+    function Lire_Nombre_Sommet(File_Name : in Unbounded_String) return Natural is
+        N : Natural;                            -- Le Nombre de Noeuds
+        File : File_type;                       -- Variable qui stocke le fichier du graphe 
+    begin
+        open(File, Name => To_string(File_Name), mode => In_File);
+
+        begin
+            -- lit le nombre de noeuds dans le fichier
+            N := Integer'Value(Get_line(File));
+        exception
+            when others => raise Bad_Arguments_Exception with "Le fichier ne respecte pas la structure demander";
+        end;
+        close(File);
+        return N;
+    end Lire_Nombre_Sommet;
+
+
+    procedure Parseur_Ligne (Line : in Unbounded_String; Depart : out Positive; Arrivee : out Positive) is
+        Caractere : Character;                              -- chaine de caractère lu par le curseur
+        long : constant Natural := length(Line);            -- La longueur de la ligne
+        I : Natural;                                        -- Itérateur sur la chaine de caractère
+    begin
+        I := 0;
+        loop
+            I:= I+1;
+            Caractere := Element(Line, I);
+            exit when Caractere = ' ' or I = long;
+        end loop;
+        Depart := Integer'Value(To_String(Line)(1..I));
+        Arrivee := Integer'Value(To_String(Line)(I..long));
+
+    end Parseur_Ligne;
+
+
+    procedure Lire_Graphe (File_Name : in Unbounded_String; Network : out Graphe.T_Graphe) is
+        File : File_type;                       -- Variable qui stocke le fichier du graphe 
+        line : Unbounded_String;                -- ligne du fichier
+        Depart : Positive;                      -- le numéro du node de départ
+        Arrivee : Positive;                     -- le numéro du node d'arrivée
+
+        N : constant Natural := Lire_Nombre_Sommet(File_Name);
+
+    begin
+        open(File, Name => To_string(File_Name), mode => In_File);
+        Skip_Line(File); -- on passe la ligne ou est affiché le nombre de sommet
+
+        Graphe.Initialiser(Network);
+        
+        loop
+            line := To_Unbounded_String(Get_Line(File));
+            Parseur_Ligne(line, Depart, Arrivee);
+            -- vérification de la cohérence des sommets données dans le fichier
+            if Depart >= N or Arrivee >= N then
+                raise Bad_Arguments_Exception;
+            else
+                null;
+            end if;
+            Graphe.Creer_Arete(Network, Depart, Arrivee);
+            exit when End_of_file(File);
+        end loop;
+
+        close(File);
+    end Lire_Graphe;
+
+end IOStream;
---
+++ src/graph/iostream.ads
@@ -0,0 +1,98 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with graph;
+
+generic
+    type T_Float is digits <>;
+    with function "<" (left : in T_Float; right : in T_Float) return Boolean;
+
+package IOStream is
+    
+    type T_Constantes;
+
+    -- Type énumération qui définie le mode de calcul
+    type T_Mode is (
+        Pleine,
+        Creuse
+    );
+
+    -- le tableau vide des arguments
+    type T_Content is array (Positive range <>) of Unbounded_String;
+    
+    -- les arguments
+    type T_Args(V_Count : Natural) is tagged record
+        V_Args : T_Content(1..V_Count);
+    end record;
+
+    -- Exception levé quand les argument ne fonctionne pas
+    Bad_Arguments_Exception : Exception;
+
+    -- Parse_Args
+    -- Traiter les paramètres et définir les constantes
+    -- Paramètres : 
+    --      - args          [in]        Le tableau des arguments données
+    --      - Constantes    [out]       Record qui contient toutes les constantes
+    -- pre :
+    --      - Auncune
+    -- post :
+    --      - Aucune
+    function Parse_Args(args : in T_Args) return T_Constantes;
+
+    -- Lire_Nombre_Sommet
+    -- retourne le nombre de sommet du graphe
+    -- Paramètres :
+    --      - Aucun
+    -- Pre :
+    --      - Aucune
+    -- Post :
+    --      - Aucune
+    function Lire_Nombre_Sommet(File_Name : in Unbounded_String) return Natural;
+
+    -- Parseur_Ligne
+    -- Decomposer la ligne "an...a0 bm...b0" en deux entier an...a0 et bm...b0
+    -- Paramètres :
+    --      - Line      [in]        La ligne qui possède les deux entier séparé par un espace
+    --      - Depart    [out]       Le premier entier
+    --      - Arrivee   [out]       Le dexième entier
+    -- Pre:
+    --      - Aucune
+    -- Post :
+    --      - Aucune
+    procedure Parseur_Ligne (Line : in Unbounded_String; Depart : out Positive; Arrivee : out Positive);
+
+    --procedure parse_file(name : Unbounded_String);
+
+    -- Lire_Graphe
+    -- lire le fichier texte associé à File_Name et retourne une matrice d'adjacence de taille N erreur si le nombre de noeud dans le fichier est supérieur à la valeur de généricité
+    -- Paramètres :
+    --      - File_Name     [in]        Le nom du fichier dont le graphe doit être extrait
+    --      - Network       [out]       La variable qui stocke le graphe
+    -- pre :
+    --      - Aucune
+    -- post :
+    --      - Aucune
+    -- Exemple :
+    --      Graphe : T_Graphe;
+    --      File_Name : Unbounded_String := To_Unbounded_String("Test.net");
+    --      Lire_graphe(File_Name, Graphe);
+    --      Graphe = ⌈ 0 1 1 1 0 ⌉
+    --               | 1 0 1 0 0 |
+    --               | 1 1 0 0 0 |
+    --               | 1 0 0 0 1 |
+    --               ⌊ 0 0 0 1 0 ⌋
+    generic
+        with package Graphe is new graph (<>);
+    procedure Lire_Graphe(File_Name : in Unbounded_String; Network : out Graphe.T_Graphe);
+
+    type T_Constantes is record
+        
+        alpha : T_Float;
+        k : Natural;
+        eps : T_Float;
+        mode : T_Mode;
+        prefixe : Unbounded_String;
+    end record;
+
+private
+
+
+end IOStream;
